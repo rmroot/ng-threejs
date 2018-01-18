7a79fc23b43b05cc1dfda67f44ed77c0221c4c06
Robert Root <rmroot@live.com>
2018-01-17 19:39:40 -0600

    please work

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const publicweb = '/';
-console.log(publicweb);
+var path = require('path');
 const app = express();
-app.use(express.static(publicweb));
-console.log(`serving ${publicweb}`);
+app.use(express.static(__dirname));
+
 app.get('*', (req, res) => {
-  res.sendFile(`index.html`, { root: publicweb });
-});
+  res.sendFile(path.join(__dirname,'index.html')
+  )
+})
 const port = process.env.SERVER_PORT || '3000';
 app.listen(port, () => console.log(`API running on localhost:${port}`));
